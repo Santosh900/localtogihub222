3861abd578059d08288a8b2c4517f00e1fefc29b
Santosh <santoshrk690@gmail.com>
2026-08-19 20:12:38 +0530

    someline shave bene adde d

---
+++ opppp.py
@@ -0,0 +1,6 @@
+Let us make some cbanges in side the this folder so that i ca ccome back and say teh 
+we are on the same page 
+
+Let us come back on the programaming that we are able o undersa 
+
+whatever te we have let us under and think o =n how we can optimize the code t hat we can reduce the cost on the memory 
